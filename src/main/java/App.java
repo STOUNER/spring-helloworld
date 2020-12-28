53538877ce629976d31a565e322651625a84b596
STOUNER <ShraikNew@gmail.com>
2020-12-29 02:55:40 +0300

    Реализация v1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld)applicationContext.getBean("helloworld");
+        HelloWorld bean_2 = (HelloWorld)applicationContext.getBean("helloworld");
+        System.out.println(bean == bean_2);
         System.out.println(bean.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean == catBean2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -6,9 +6,19 @@ import org.springframework.context.annotation.Scope;
 public class AppConfig {
  
     @Bean(name="helloworld")
+    //Знаю, что по умолчанию Scope = singleton, но не уверен, что это так-же и в настроечном классе, как и в XML.
+    @Scope("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setMessage("Mew");
+        return cat;
+    }
+
 }
